ada134752f9c335187d1a710cd5ea3f7f6c8b895
jingwenzheng <alphabetion@gmail.com>
2021-02-09 10:48:28 +0800

    feat: 第三方依赖包

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -4,3 +4,19 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+
+// declaration.d.ts
+declare module '*.module.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+// declaration.d.ts
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "*.png";
+declare module "*.svg";
